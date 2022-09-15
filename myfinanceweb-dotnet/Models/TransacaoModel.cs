215c265a0302d8ddb6f4dc4d424e6a4d8f0157dd
Jonathan <s.mieldazis@hotmail.com>
2022-09-15 20:21:33 -0300

    fix launch settings

--- myfinanceweb-dotnet/Models/TransacaoModel.cs
+++ myfinanceweb-dotnet/Models/TransacaoModel.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace myfinance_web_netcore.Models
+namespace myfinanceweb_dotnet.Models
 {
     public class TransacaoModel
     {
         public int? Id { get; set; }
         public DateTime Data { get; set; }
         public decimal Valor { get; set; }
-        public string? Historico { get; set; }
-        public string? Tipo { get; set; }
+        public string Historico { get; set; }
+        public string Tipo { get; set; }
+        [Display(Name = "Plano de Contas")]
         public int IdPlanoConta { get; set; }
+        public IEnumerable<SelectListItem> PlanoContas { get; set; }
     }
 }
